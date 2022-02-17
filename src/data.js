17d0b2e154f9187de84841ed510c6520c10f6f83
Talha Y <tycode0@gmail.com>
2022-02-17 18:45:00 +0100

    updated Readme

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ import { getCoordinates } from "./pages/inputPage";
 export async function fetchData(url) {
   const response = await fetch(url, {
     headers: {
-      Authorization: 'Bearer 18bf0711-9335-44f9-ab93-23ce2ac046d',
+      Authorization: '',
     },
   });
   if (!response.ok) {
